bd4902fce9590bb81643b0a3bc1e9fa77aff716c
Jesse Millar <hellojessemillar@gmail.com>
2017-04-11 19:12:38 -0600

    Fixing weird spacing

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,3 @@
-
-
 /*=============================================================
     Authour URI: www.binarytheme.com
     License: Commons Attribution 3.0
@@ -12,58 +10,57 @@
     ========================================================  */
 
 
-(function ($) {
-    "use strict";
-    var mainApp = {
+(function($) {
+	"use strict";
+	var mainApp = {
 
-        main_fun: function () {
-            /*====================================
-              CUSTOM LINKS SCROLLING FUNCTION 
-             ======================================*/
+		main_fun: function() {
+			/*====================================
+			  CUSTOM LINKS SCROLLING FUNCTION 
+			 ======================================*/
 
-            $('.navbar a[href*=#]').click(function () {
-                if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
-               && location.hostname == this.hostname) {
-                    var $target = $(this.hash);
-                    $target = $target.length && $target
-                    || $('[name=' + this.hash.slice(1) + ']');
-                    if ($target.length) {
-                        var targetOffset = $target.offset().top;
-                        $('html,body')
-                        .animate({ scrollTop: targetOffset }, 800); //set scroll speed here
-                        return false;
-                    }
-                }
-            });
+			$('.navbar a[href*=#]').click(function() {
+				if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
+					location.hostname == this.hostname) {
+					var $target = $(this.hash);
+					$target = $target.length && $target ||
+						$('[name=' + this.hash.slice(1) + ']');
+					if ($target.length) {
+						var targetOffset = $target.offset().top;
+						$('html,body')
+							.animate({
+								scrollTop: targetOffset
+							}, 800); //set scroll speed here
+						return false;
+					}
+				}
+			});
 
-       
-            // CAROUSEL FUNCTION 
 
-            $('.carousel').carousel({
-                interval: 2000 //TIME IN MILLI SECONDS
-            })
+			// CAROUSEL FUNCTION 
 
-          
-            /*====================================
+			$('.carousel').carousel({
+				interval: 2000 //TIME IN MILLI SECONDS
+			})
+
+
+			/*====================================
                WRITE YOUR SCRIPTS BELOW 
            ======================================*/
 
 
-        },
+		},
 
-        initialization: function () {
-            mainApp.main_fun();
+		initialization: function() {
+			mainApp.main_fun();
 
-        }
+		}
 
-    }
-    // Initializing ///
+	}
+	// Initializing ///
 
-    $(document).ready(function () {
-        mainApp.main_fun();
-    });
+	$(document).ready(function() {
+		mainApp.main_fun();
+	});
 
 }(jQuery));
-
-
-
